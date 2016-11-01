732e13345d5d42c9e0c9a3cf533ee387a613e9c3
stevehepton <steve.hepton@gmail.com>
2016-11-01 16:24:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 (function() {
   var app = {
-    // views will be defined here
+    // The views are defined here
     'views': {
+      'another-view': {
+        'rendered': function() {
+          console.log('this view is "another-view"');
+          app.viewElem.innerHTML = '<p>This javascript content overrides the static content for this view.</p>';
+        }
+      },
+      'yet-another-view': {
+        'rendered': function() {
+          console.log('this view is "yet-another-view"');
+          app.viewElem.innerHTML = '<p>This javascript content overrides the static content for this view.</p>';
+        }
+      }
     },
-    // start the app.
+    // The default view is defined here.
+    // TODO: We could query the DOM to define it on the fly?
+    'default': 'the-default-view',
+    'viewChange': function() {
+      app.viewID = location.hash.slice(1);
+      app.view = app.views[app.viewID];
+      app.viewElem = document.getElementById(app.viewID);
+      if (app.view) {
+        app.view.rendered();
+      }
+    },
+    // Start the app.
     'init': function() {
     }
   };
