6d6f2cadd04a0cfc0d7523dd35cf90b6c4a05649
stevehepton <steve.hepton@gmail.com>
2016-11-01 14:45:32 +0000

    Create app.js
    Initial check-in of app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+(function() {
+  var app = {
+    // views will be defined here
+    'views': {
+    },
+    // start the app.
+    'init': function() {
+    }
+  };
+  window.app = app;
+})();
+
+app.init();
